ac62d03cb76ef27fa2850250a318a8c13890a8ae
faiyuching <faiyuching@magic.local>
2021-03-27 14:51:35 +0800

    update

--- src/pages/response/ResponseAbout.tsx
+++ src/pages/response/ResponseAbout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import {
-  IonContent, IonHeader, IonMenuButton, IonTextarea,
-  IonPage, IonTitle, IonToolbar, IonSplitPane,
-  IonButton, IonButtons, IonItem, IonLabel, IonInput,
+  IonContent, IonHeader, IonMenuButton,
+  IonPage, IonTitle, IonToolbar, IonSplitPane, IonIcon,
+  IonButtons, IonItem, IonLabel, IonItemGroup,
+  IonItemSliding, IonItemOptions, IonItemOption,
 } from '@ionic/react';
+import { pencilOutline } from 'ionicons/icons';
 import ResponseMenu from '../../components/response/ResponseMenu';
 import { useTranslation } from "react-i18next";
 
@@ -22,48 +24,129 @@ const ResponseAbout: React.FC = () => {
           </IonToolbar>
           <IonToolbar>
             <IonTitle size="large">{t("response.about")}</IonTitle>
-            <IonButtons slot="end">
-              <IonButton>{t("response.edit")}</IonButton>
-            </IonButtons>
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
-          <IonItem>
-            <IonLabel position="stacked">{t("response.response_name")}</IonLabel>
-            <IonInput value={"text"}> </IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="stacked">{t("response.organizer")}</IonLabel>
-            <IonInput value={"text"}> </IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="stacked">{t("response.disaster_type")}</IonLabel>
-            <IonInput value={"text"}> </IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="stacked">{t("response.disaster_level")}</IonLabel>
-            <IonInput value={"text"}> </IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="stacked">{t("response.begin_time")}</IonLabel>
-            <IonInput value={"text"}> </IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="stacked">{t("response.needs_time")}</IonLabel>
-            <IonInput value={"text"}> </IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="stacked">{t("response.end_time")}</IonLabel>
-            <IonInput value={"text"}> </IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="stacked">{t("response.need_people")}</IonLabel>
-            <IonInput value={"text"}> </IonInput>
-          </IonItem>
-          <IonItem>
-            <IonLabel position="stacked">{t("response.statement")}</IonLabel>
-            <IonTextarea autoGrow value={"text"}> </IonTextarea>
-          </IonItem>
+          <IonItemGroup>
+            <IonItemSliding>
+              <IonItem lines="full">
+                <IonLabel className="ion-text-wrap">
+                  <h2>{t("response.response_name")}</h2>
+                  <p>text</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption>
+                  <IonIcon slot="icon-only" icon={pencilOutline} />
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+            <IonItemSliding>
+              <IonItem lines="full">
+                <IonLabel className="ion-text-wrap">
+                  <h2>{t("response.organizer")}</h2>
+                  <p>text</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption>
+                  <IonIcon slot="icon-only" icon={pencilOutline} />
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+            <IonItemSliding>
+              <IonItem lines="full">
+                <IonLabel className="ion-text-wrap">
+                  <h2>{t("response.disaster_type")}</h2>
+                  <p>text</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption>
+                  <IonIcon slot="icon-only" icon={pencilOutline} />
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+            <IonItemSliding>
+              <IonItem lines="full">
+                <IonLabel className="ion-text-wrap">
+                  <h2>{t("response.disaster_level")}</h2>
+                  <p>text</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption>
+                  <IonIcon slot="icon-only" icon={pencilOutline} />
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+            <IonItemSliding>
+              <IonItem lines="full">
+                <IonLabel className="ion-text-wrap">
+                  <h2>{t("response.begin_time")}</h2>
+                  <p>text</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption>
+                  <IonIcon slot="icon-only" icon={pencilOutline} />
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+            <IonItemSliding>
+              <IonItem lines="full">
+                <IonLabel className="ion-text-wrap">
+                  <h2>{t("response.needs_time")}</h2>
+                  <p>text</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption>
+                  <IonIcon slot="icon-only" icon={pencilOutline} />
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+            <IonItemSliding>
+              <IonItem lines="full">
+                <IonLabel className="ion-text-wrap">
+                  <h2>{t("response.end_time")}</h2>
+                  <p>text</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption>
+                  <IonIcon slot="icon-only" icon={pencilOutline} />
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+            <IonItemSliding>
+              <IonItem lines="full">
+                <IonLabel className="ion-text-wrap">
+                  <h2>{t("response.need_people")}</h2>
+                  <p>text</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption>
+                  <IonIcon slot="icon-only" icon={pencilOutline} />
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+            <IonItemSliding>
+              <IonItem lines="full">
+                <IonLabel className="ion-text-wrap">
+                  <h2>{t("response.statement")}</h2>
+                  <p>Multiline text that should wrap when it is too long
+                  to fit on one line in the item...</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side="end">
+                <IonItemOption>
+                  <IonIcon slot="icon-only" icon={pencilOutline} />
+                </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+          </IonItemGroup>
         </IonContent>
       </IonPage>
     </IonSplitPane>
